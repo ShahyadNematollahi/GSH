1a1bd2df93d7424889dd57b799aa7749686a38f6
shahyadgsh <shahyadgsh@gmail.com>
2016-03-05 20:29:02 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {},--Sudo users
+    sudo_users = {103015883},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
